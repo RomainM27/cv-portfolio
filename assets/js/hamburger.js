663647d1c02c6f8109b763784f0cb38c220e759e
RomainM27 <70415526+RomainM27@users.noreply.github.com>
2021-02-16 11:44:04 +0100

    menu amelioration

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -1,6 +1,8 @@
 
 (() => {
 	const hamburgerButtons = document.querySelectorAll('.hamburger-button');
+	const mobileMenu = document.querySelector(".mobile")
+	const mobileMenuBg = document.querySelector(".mobile_menu-bg")
 
 	hamburgerButtons.forEach(button => {
 		button.addEventListener('click', hamburgerToggle);
@@ -10,6 +12,8 @@
 		let button = this;
 		button.classList.add('animation');
 		button.classList.toggle('active');
+		mobileMenu.classList.toggle("hidden")
+		mobileMenuBg.classList.toggle("mobile_menu-bg-open")
 
 		let clone = button.cloneNode(true);
 		button.parentNode.replaceChild(clone, button);
