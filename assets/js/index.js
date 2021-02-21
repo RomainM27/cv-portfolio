df632e8b120f06a56163fc70ce850f7ddbadab81
RomainM27 <romain.mathieu96@gmail.com>
2021-02-21 19:35:47 +0100

    menu fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,46 +5,37 @@ window.addEventListener('DOMContentLoaded', () => {
         header.classList.toggle("sticky", window.scrollY > 0);
         logoColor.classList.toggle("white",window.scrollY > 0);
     })
-    // intersection observer setup
-    const observerOptions = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.1, // si 10% de l ecran est occupe par l element
-    };
-    const observer = new IntersectionObserver(entries => {
+
+
+    // simple function to use for callback in the intersection observer
+    const changeNav = (entries, observer) => {
         entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                // get the nav item corresponding to the id of the section
-                // that is currently in view
-                const navItem = entry.target.getAttribute('id');
-                // add 'active' class on the navItem
+            // verify the element is intersecting
+            if(entry.isIntersecting && entry.intersectionRatio >= 0.45) {
+                // remove old active class
+                document.querySelector('ul li .active').classList.remove('active');
+                // get id of the intersecting section
+                var id = entry.target.getAttribute('id');
+                // find matching link & add appropriate class
                 if (992 < window.visualViewport.width){
-                    document.querySelector(`ul li a[href="#${navItem}"]`).classList.add('active');
+                    var newLink = document.querySelector(`[href="#${id}"]`).classList.add('active');
                 }else{
-                    document.querySelector(`.mobile li a[href="#${navItem}"]`).classList.add('active');
+                    var newLink = document.querySelector(`.mobile [href="#${id}"]`).classList.add('active');
                 }
-                // remove 'active' class from any navItem that is not
-                // same as 'navItem' defined above
-                if (992 < window.visualViewport.width){
-                    document.querySelectorAll("header nav ul li a").forEach((item) => {
-                        if (item.getAttribute("href") != `#${navItem}`) {
-                        item.classList.remove('active');
-                        }
-                    });
-                }else{
-                    document.querySelectorAll(".mobile li a").forEach((item) => {
-                        if (item.getAttribute("href") != `#${navItem}`) {
-                        item.classList.remove('active');
-                        }
-                    });
-                }
-               
             }
-          });
-      }, observerOptions);
-    
-      // Track all sections that have an `id` applied
-    document.querySelectorAll('section[id]').forEach((section) => {
+        });
+    }
+
+    // init the observer
+    const options = {
+        threshold: 0.45
+    }
+
+    const observer = new IntersectionObserver(changeNav, options);
+
+    // target the elements to be observed
+    const sections = document.querySelectorAll('section');
+    sections.forEach((section) => {
         observer.observe(section);
     });
 });
